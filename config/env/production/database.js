b3c688b05f7d699f341683aa24967f0f9441e122
kevinusmayadhyw <57991938+kevinusmayadhyw@users.noreply.github.com>
2022-07-01 14:21:28 +0700

    Update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,31 +1,19 @@
-const parse = require("pg-connection-string").parse;
-
+const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
-
+    client: 'postgres',
     connection: {
       host: config.host,
-
       port: config.port,
-
       database: config.database,
-
       user: config.user,
-
       password: config.password,
-
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        rejectUnauthorized: false
       },
     },
-
-    options: {
-      ssl: env.bool("DATABASE_SSL", false),
-    },
-
     debug: false,
   },
 });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,17 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
-
-  url: env("MY_HEROKU_URL"),
-
-  app: {
-    keys: env.array("APP_KEYS", [
-      "APPKEYS1==",
-
-      "APPKEYS2==",
-
-      "APPKEYS3==",
-
-      "APPKEYS4==",
-    ]),
+  url: env('MY_HEROKU_URL'),
+  app: { 
+    keys: env.array('APP_KEYS')
   },
-});
+})
